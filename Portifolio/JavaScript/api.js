ff8a76872dd5a5521352a9cfa71fab9291a30e94
YorranG <yorrangustavo@gmail.com>
2023-09-11 19:36:12 -0300

    final
    Final

---
+++ Portifolio/JavaScript/api.js
@@ -0,0 +1,7 @@
+
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/MarcosWinther/project-portfolio-html-css-js/main/data/profile.json';
+    const fetching = await fetch(url);
+    return await fetching.json();
+ }
